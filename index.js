da8f42d7491eaa453d7599bdd64852e429c1524a
sara ahmed <sara.bakr.ahmad@gmail.com>
2022-10-29 22:04:27 +0300

    done with it

--- index.js
+++ index.js
@@ -1,11 +1,80 @@
-function scuberGreetingForFeet(){
+
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance>2500){
+    return "No can do.";
+  }
+  if(distance<=400){
+  return "This one is on me!";
+
+ } else if(distance>400 && distance<2000){
+    return "That will be twenty bucks."
+  }
+
+  else if(distance>200){
+return "I will gladly take your thirty bucks.";
+  }
+
+
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+if(city=="NYC"){
+  return "Ok, sounds good.";
+}
+else{
+  return "No go.";
+}
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip=="generous"){
+    return "Thank you so much.";
+  }
+  else if(tip=="not as generous"){
+    return "Thank you.";
+  }
+  else{
+    return "Bye."
+  }
 }
+
+
+
+
+
+/*
+function scuberGreetingForFeet(rideLength){
+  if (rideLength <= 400) {
+      return "This one is on me!";
+  }
+  
+  if (rideLength >2500) {
+      return "No can do.";
+  } else if (rideLength >= 2000) {
+      return "I will gladly take your thirty bucks.";
+  }
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good.": "No go.";
+}
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+      case 'generous' :
+          return 'Thank you so much.';
+          break;
+      case 'not as generous' :
+          return 'Thank you.';
+          break;
+      default:
+          return 'Bye.';
+          break;
+  }
+}
+*/
